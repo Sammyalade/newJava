03f713f07ea83d002dde19e5ccff99fbf370fd21
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-10 23:21:00 +0100

    Tasks

--- src/ArrayList/ArrayList.java
+++ src/ArrayList/ArrayList.java
@@ -1,34 +1,119 @@
 package ArrayList;
 
-import java.util.Objects;
+import java.util.Arrays;
 
 public class ArrayList {
 
     private int countOfElements;
-    private final String[] elements = new String[3];
+    private String[] temporary = {};
+
+    private String[] elements = {};
     public boolean isEmpty() {
         return countOfElements == 0;
     }
 
     public void add(String element) {
+        createOneMoreContainer();
         elements[countOfElements] = element;
         countOfElements++;
     }
 
     public void remove(String element) {
-        if (countOfElements == 0){
+        if (countOfElements == 0) {
             throw new ArrayIndexOutOfBoundsException("ArrayList is Empty");
         }
+        elements = deleteUnusedContainer(element);
         countOfElements--;
     }
 
     public int get(String element) {
         int count = 0;
-        for (int index = 0; index < elements.length; index++) {
-            if (elements[index].equals(element)) {
-                count = index;
-            }
+        for(int index = 0; index < elements.length; index++){
+            if(element.equals(elements[index]))
+                count = index+1;
         }
         return count;
+    }
+
+    public String get(int index) {
+        for (String element : elements) {
+            if (elements[index-1].equals(element)) return element;
+        }
+        return null;
+    }
+
+    public int getSize() {
+        return countOfElements;
+    }
+
+    public void insert(String element, int index) {
+        if (elements[elements.length-1] != null){
+            createOneMoreContainer();
+        }
+        if(elements[index-1] == null) {
+            elements[index - 1] = element;
+        } else {
+            String temp = "";
+
+            for (int indexOfElement = elements.length-1; indexOfElement > index-1 ; indexOfElement--){
+                if(indexOfElement-1 != index -1){
+                    temp = elements[indexOfElement-1];
+                    elements[indexOfElement] = temp;
+                    continue;
+                } else {
+                    temp = elements[indexOfElement-1];
+                    elements[indexOfElement] = temp;
+                    elements[index-1] = element;
+                }
+
+
+            }
+        }
+        countOfElements++;
+        System.out.println(toString());
+
+    }
+
+    public void createOneMoreContainer(){
+        temporary = new String[countOfElements+1];
+        int count = 0;
+        while (count < temporary.length-1) {
+            temporary[count] = elements[count];
+            count++;
+        }
+        elements = temporary;
+    }
+
+    public String display() {
+        return String.format(Arrays.toString(elements));
+    }
+
+    @Override
+    public String toString(){
+        String concatenate = "";
+        for(int count = 0; count < countOfElements; count++){
+            if(count != countOfElements-1){
+                concatenate += elements[count] + "," + " ";
+            } else {
+                concatenate += elements[count];
+            }
+        }
+        return String.format("(%s)", concatenate);
+    }
+
+    public String[] deleteUnusedContainer(String element){
+        String[] temp = new String[countOfElements-1];
+        for (int count = 0; count < countOfElements-1; count++) {
+            boolean isDuplicate = count != 0 && temp[count-1].equals(elements[count]);
+            if (elements[count].equals(element)) {
+                temp[count] = elements[count+1];
+                continue;
+            } else if(isDuplicate){
+                temp[count] = elements[count+1];
+            } else {
+                temp[count] = elements[count];
+            }
+        }
+        return  temp;
     }
 }
--- test/ArrayListTest/ArrayListTest.java
+++ test/ArrayListTest/ArrayListTest.java
@@ -35,7 +35,7 @@ public class ArrayListTest {
     }
 
     @Test
-    public void addTowElementsAndRemoveOne_listIsEmptyTest(){
+    public void addTwoElementsAndRemoveOne_listIsEmptyTest(){
         assertTrue(string.isEmpty());
         string.add("Django");
         string.add("Python");
@@ -56,7 +56,72 @@ public class ArrayListTest {
         assertTrue(string.isEmpty());
         string.add("Django");
         assertFalse(string.isEmpty());
-        assertEquals(1, string.get("Django"));
+        assertEquals(1, string. get("Django"));
+    }
+
+    @Test
+    public void addOneElement_getByIndexTest(){
+        assertTrue(string.isEmpty());
+        string.add("Django");
+        assertEquals("Django", string.get(1));
+    }
+
+    @Test
+    public void getListSize_listReturnsSize(){
+        string.add("Django");
+        string.add("Python");
+        string.add("Ruby");
+        assertEquals(3, string.getSize());
+    }
+
+    @Test
+    public void insertItemInAList_itemInsertSuccessfully(){
+        string.add("Java");
+        string.add("Kotlin");
+        string.add("Ruby");
+        string.insert("Django", 3);
+        assertEquals(1, string.get("Java"));
+        assertEquals(2, string.get("Kotlin"));
+        assertEquals(3 , string.get("Django"));
+        assertEquals(4 , string.get("Ruby"));
+
+    }
+
+    @Test
+    public void testThatContainerSizeCanIncrease(){
+        string.add("Java");
+        string.add("C++");
+        string.add("Python");
+        string.add("Kotlin");
+        assertEquals(2, string.get("C++"));
+    }
+
+    @Test
+    public void displayAllElementInArrayListTest(){
+        string.add("Java");
+        string.add("C++");
+        string.add("Python");
+        string.add("Kotlin");
+        assertEquals("[Java, C++, Python, Kotlin]", string.display());
+    }
+
+    @Test
+    public void testThatArrayListToStringMethod(){
+        string.add("Java");
+        string.add("C++");
+        string.add("Python");
+        string.add("Kotlin");
+        assertEquals("(Java, C++, Python, Kotlin)", string.toString());
+    }
+
+    @Test
+    public void removeAnItem_checkThatTheContainerReducedTest(){
+        string.add("Java");
+        string.add("C++");
+        string.add("Python");
+        string.add("Kotlin");
+        string.remove("Java");
+        assertEquals("(C++, Python, Kotlin)", string.toString());
     }
 
 }
