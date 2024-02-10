4ea0ef2682c9079686ff01a6ea59e043c2e522eb
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-11 00:23:26 +0100

    Tasks

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,4 +1,39 @@
 package stack;
 
 public class Stack {
+    private int itemSize;
+    private int[] itemContainer = {};
+    private int[] temporary = {};
+
+    public boolean isEmpty() {
+        return itemSize == 0;
+    }
+
+    public void push(int elementToPush) {
+        createOneMoreContainer();
+        itemContainer[itemSize] = elementToPush;
+        itemSize++;
+    }
+
+    public int getSize() {
+        return itemSize;
+    }
+
+    public void pop() {
+        itemSize--;
+    }
+
+    public int peek() {
+        return itemContainer[itemSize-1];
+    }
+
+    public void createOneMoreContainer(){
+        temporary = new int[itemSize+1];
+        int count = 0;
+        while (count < temporary.length-1) {
+            temporary[count] = itemContainer[count];
+            count++;
+        }
+        itemContainer = temporary;
+    }
 }
--- test/stackTest/StackTest.java
+++ test/stackTest/StackTest.java
@@ -1,4 +1,73 @@
 package stackTest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import stack.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class StackTest {
+
+    Stack myStack;
+
+    @BeforeEach
+    public void initializeMyStack(){
+        myStack = new Stack();
+    }
+    @Test
+    public void checkIfStackIsEmpty_stackIsEmptyTest(){
+        assertTrue(myStack.isEmpty());
+    }
+
+    @Test
+    public void pushToMyStack_pushSuccessful(){
+        assertTrue(myStack.isEmpty());
+        myStack.push(50);
+        assertEquals(1, myStack.getSize());
+    }
+
+    @Test
+    public void pushTwoItemsToStack_itemsPushedSuccessfully(){
+        assertTrue(myStack.isEmpty());
+        myStack.push(50);
+        myStack.push(40);
+        assertEquals(2, myStack.getSize());
+    }
+
+    @Test
+    public void popItemsFromStack_itemsPopSuccessfully(){
+        assertTrue(myStack.isEmpty());
+        myStack.push(50);
+        myStack.push(40);
+        myStack.pop();
+        assertEquals(1, myStack.getSize());
+    }
+
+    @Test
+    public void peekItem__returnsLastItemPushed(){
+        assertTrue(myStack.isEmpty());
+        myStack.push(50);
+        myStack.push(40);
+        assertEquals(40, myStack.peek());
+    }
+
+    @Test
+    public void checkThatAfterPush_itemContainerIncreasedTest(){
+        assertTrue(myStack.isEmpty());
+        myStack.push(50);
+        myStack.push(40);
+        myStack.push(30);
+        myStack.push(20);
+        assertEquals(4, myStack.getSize());
+    }
+
+    @Test
+    public void popLastItemCheckItem_itemEqualsToTheNextToLastTest(){
+        myStack.push(50);
+        myStack.push(40);
+        myStack.push(30);
+        myStack.pop();
+        assertEquals(40, myStack.peek());
+    }
 }
