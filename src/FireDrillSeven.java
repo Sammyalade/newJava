6d47ef1ab8a0abd874730f31a7596c115fd70718
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-15 11:17:00 +0100

    Tasks

---
+++ src/FireDrillSeven.java
@@ -0,0 +1,14 @@
+public class FireDrillSeven {
+
+    public static boolean[] returnTrueOrFalseArray(int[] inputArrays){
+        boolean[] outPutArray = new boolean[inputArrays.length];
+        for(int index = 0; index < inputArrays.length; index++){
+            outPutArray[index] = checkOddOrEven(inputArrays[index]);
+        }
+        return outPutArray;
+    }
+
+    public static boolean checkOddOrEven(int number){
+        return number % 2 != 0;
+    }
+}
