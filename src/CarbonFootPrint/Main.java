cf27b486c2c484c5da7e504f62375d8b01463261
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-16 15:48:13 +0100

    Tasks

---
+++ src/CarbonFootPrint/Main.java
@@ -0,0 +1,31 @@
+package CarbonFootPrint;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Building building = new Building(10, 50, 20);
+        Car car = new Car(8.5, 250);
+        Bicycle bicycle = new Bicycle("34545", "q2881u");
+
+        ArrayList<CarbonFootPrint> carbonFootprints = new ArrayList<>();
+        carbonFootprints.add(building);
+        carbonFootprints.add(car);
+        carbonFootprints.add(bicycle);
+
+        for (CarbonFootPrint carbonFootprint : carbonFootprints) {
+            if (carbonFootprint instanceof Building) {
+                System.out.println("Building:");
+                System.out.println("Carbon Footprint: " + carbonFootprint.returnCarbonFootPrint() + " kg of CO2");
+            } else if (carbonFootprint instanceof Car) {
+                System.out.println("Car:");
+                System.out.println("Carbon Footprint: " + carbonFootprint.returnCarbonFootPrint() + " g of CO2 per km");
+            } else if (carbonFootprint instanceof Bicycle) {
+                System.out.println("Bicycle:");
+                System.out.println("Carbon Footprint: " + carbonFootprint.returnCarbonFootPrint() + " kg of CO2 (assumed negligible)");
+            }
+            System.out.println("-----------------------------");
+        }
+    }
+}
