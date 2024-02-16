c5d296163df3de745a8d6f5b88456602c7e7cbb9
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-16 15:15:55 +0100

    Tasks

--- src/CarbonFootPrint/Bicycle.java
+++ src/CarbonFootPrint/Bicycle.java
@@ -1,12 +1,33 @@
 package CarbonFootPrint;
 
 public class Bicycle implements CarbonFootPrint{
-
     private String brand;
-    private String model;
-    @Override
-    public void getCarbonFootPrint() {
 
+    private String model;
+
+    public Bicycle(String brand, String model){
+        this.brand = brand;
+        this.model = model;
+    }
+
+    @Override
+    public double calculateCarbonFootprint() {
+        double manufacturingEmissions = 50;
+        double maintenanceEmissions = 10;
+
+        return manufacturingEmissions + maintenanceEmissions;
+    }
+
+    @Override
+    public double getCarbonFootPrint() {
+        return 0;
+    }
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
     }
 
 }
