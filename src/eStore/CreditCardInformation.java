6732008f97ee496a0cb4e9485aa21e512785417a
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-18 09:44:06 +0100

    Tasks

---
+++ src/eStore/CreditCardInformation.java
@@ -0,0 +1,11 @@
+package eStore;
+
+public class CreditCardInformation {
+
+    private String cardCVV;
+    private String cardExpirationYear;
+    private String cardExpirationMonth;
+    private String cardNumber;
+    private String cardName;
+    private CardType cardType;
+}
