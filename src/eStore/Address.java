c3f9f2fbb43c892b7c6f0d6c4801fa8312340b79
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-18 09:42:32 +0100

    Tasks

---
+++ src/eStore/Address.java
@@ -0,0 +1,10 @@
+package eStore;
+
+public class Address {
+
+    private String cityName;
+    private String country;
+    private String houseNumber;
+    private String street;
+    private String state;
+}
