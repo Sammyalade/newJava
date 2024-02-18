6f16c946ea9da28ef932a7665d685b33dc5321ab
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-18 09:45:22 +0100

    Tasks

---
+++ src/eStore/Product.java
@@ -0,0 +1,10 @@
+package eStore;
+
+public class Product {
+
+    private String productID;
+    private String productName;
+    private int price;
+    private String productDescription;
+    private ProductCategory productCategory;
+}
