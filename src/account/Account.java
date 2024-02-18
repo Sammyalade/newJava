f8cd6ebfe82404e4e0530845955c539cf4f460c5
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-18 10:00:50 +0100

    Tasks

---
+++ src/account/Account.java
@@ -0,0 +1,17 @@
+package account;
+
+public class Account {
+
+    private String name;
+    private int balance;
+    private String pin;
+    private int number;
+
+    public void deposit(int Amount){
+
+    }
+
+    public void withdraw(int amount, String reason){
+
+    }
+}
