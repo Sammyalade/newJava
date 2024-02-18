93e961c26d923bde0b21d2e6bdaa3445d093615a
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-18 09:42:45 +0100

    Tasks

---
+++ src/eStore/BillingInformation.java
@@ -0,0 +1,10 @@
+package eStore;
+
+public class BillingInformation {
+
+    private String receiverPhoneNumber;
+
+    private String receiverName;
+    private  Address deliveryAddress;
+    private CreditCardInformation cardInformation;
+}
