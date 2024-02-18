4caa28d50eeb98d864b13838c02becec00727560
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-18 09:46:20 +0100

    Tasks

---
+++ src/eStore/User.java
@@ -0,0 +1,13 @@
+package eStore;
+
+public class User {
+    private int age;
+    private String emailAddress;
+    private String homeAddress;
+    private String name;
+    private String password;
+    private String phoneNumber;
+    private Customer customer;
+    private Seller seller;
+    private Admin admin;
+}
