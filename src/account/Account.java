cc804245ccbbc77f2e1211bccd8fed708a77ce71
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-19 14:10:34 +0100

    Tasks

--- src/account/Account.java
+++ src/account/Account.java
@@ -7,15 +7,22 @@ public class Account {
     private String pin;
     private int number;
 
-    public void deposit(int Amount){
-
+    public void deposit(int amount) throws InvalidAmountException {
+        if (balance < 0){
+            throw new InvalidAmountException("Invalid Amount to deposit");
+        }
     }
 
-    public void withdraw(int amount, String reason){
-
+    public void withdraw(int amount, String reason) throws InsufficientFundsException {
+        if (amount > balance){
+            throw new InsufficientFundsException("Insufficient Funds");
+        }
     }
 
-    public int checkBalance(String pin){
+    public int checkBalance(String pin) throws InvalidPinException {
+        if (this.pin.equals(pin)){
+            throw new InvalidPinException("Invalid Pin");
+        }
         return 0;
     }
 }
