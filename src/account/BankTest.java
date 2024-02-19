0109f2713df4e5ba34607eb5d4cd0fda7f675a67
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-19 23:05:51 +0100

    Tasks

--- src/account/BankTest.java
+++ src/account/BankTest.java
@@ -1,5 +1,26 @@
 package account;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class BankTest {
 
+    private Bank myBank;
+
+    @BeforeEach
+    public void initializeMyBank(){
+        myBank = new Bank();
+    }
+
+    @Test
+    public void registerNewAccount_newAccountRegisteredTest(){
+        Account myAccount = myBank.registerCustomer("Onome", "Precious", "4455");
+
+        assertNotNull(myAccount);
+    }
+
+    @Test
+    public void
 }
