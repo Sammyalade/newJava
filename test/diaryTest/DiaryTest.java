89321cae8ff1292bfbba709f229ae025e79c67b9
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-25 09:34:14 +0100

    Tasks

---
+++ test/diaryTest/DiaryTest.java
@@ -0,0 +1,14 @@
+package diaryTest;
+
+import diaryApp.Diary;
+import org.junit.jupiter.api.Test;
+
+public class DiaryTest {
+
+    @Test
+    public void testThatUserCanPutEntryInADiary(){
+        Diary myDiary = new Diary("Username", "Password");
+        myDiary.entries
+
+    }
+}
