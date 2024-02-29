575cec3b18284745e38c1baba514eb87183ed451
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-29 04:50:37 +0100

    Tasks

--- src/SevenSegmentDisplay.java
+++ src/SevenSegmentDisplay.java
@@ -28,14 +28,28 @@ public class SevenSegmentDisplay {
         System.out.println("*       *");
     }
 
-    public void verticalSwitch(int switch1, int switch2){
+    public void checkVerticalSwitchToPrint(int switch1, int switch2){
         if (switch1 == 1 && switch2 == 1) verticalPrintLeftAndRight();
         else if (switch1 == 0 && switch2 == 1) verticalPrintLeft();
         else if (switch1 == 1 && switch2 == 0) verticalPrintRight();
     }
 
     public void checkHorizontalSwitchToPrint(int switch1){
-        if (switch1
+        if (switch1 == 1){
+            horizontalPrint();
+        }
+    }
+
+    public void printIt(){
+        int[] entries = putEntriesInAnArray();
+        if ()
+    }
+
+    private int[] putEntriesInAnArray() {
+        int[] array = null;
+        for(int index = 0; index < 9; index ++){
+            int lastNumber = switches % 10;
+        }
     }
 
 }
