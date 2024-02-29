6f73a23bb01bec4e7e031dd888041a9e1ba14d51
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-29 05:04:57 +0100

    Tasks

--- src/SevenSegmentDisplay.java
+++ src/SevenSegmentDisplay.java
@@ -1,8 +1,8 @@
 public class SevenSegmentDisplay {
 
-    private final int switches;
+    private final String switches;
 
-    public SevenSegmentDisplay(int switches){
+    public SevenSegmentDisplay(String switches){
         this.switches = switches;
     }
 
@@ -40,16 +40,28 @@ public class SevenSegmentDisplay {
         }
     }
 
-    public void printIt(){
-        int[] entries = putEntriesInAnArray();
-        if ()
+    public void switchOn() throws Exception {
+        int[] entries = putEntriesInAnArray(switches);
+        if (entries[7] == 1){
+            checkHorizontalSwitchToPrint(entries[0]);
+            checkVerticalSwitchToPrint(entries[1], entries[5]);
+            checkHorizontalSwitchToPrint(6);
+            checkVerticalSwitchToPrint(entries[3], entries[5]);
+        } else {
+            System.out.println("Main switch is off");
+        }
     }
 
-    private int[] putEntriesInAnArray() {
-        int[] array = null;
-        for(int index = 0; index < 9; index ++){
-            int lastNumber = switches % 10;
+    private int[] putEntriesInAnArray(String switch1) throws Exception {
+        int[] array = new int[switch1.length()];
+        for (int index = 0; index < switch1.length(); index++) {
+            array[index] = Character.getNumericValue(switch1.charAt(index));
+            if (index > 8){
+                throw new Exception("Input should not be more than 8");
+            }
         }
+
+        return array;
     }
 
 }
