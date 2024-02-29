743ab2b7fa927f4bf0eb166a18d7c4566d43afce
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-29 05:27:59 +0100

    Tasks

--- src/SevenSegmentDisplayTest.java
+++ src/SevenSegmentDisplayTest.java
@@ -1,6 +1,6 @@
 public class SevenSegmentDisplayTest {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SevenSegmentDisplay display = new SevenSegmentDisplay("11110111");
         display.switchOn();
     }
