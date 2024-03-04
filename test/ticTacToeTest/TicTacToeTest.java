b2ef52b9cf2ed8b946ea135f9e282cac54c5c4e3
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-04 09:00:13 +0100

    Tasks

---
+++ test/ticTacToeTest/TicTacToeTest.java
@@ -0,0 +1,15 @@
+package ticTacToeTest;
+
+import org.junit.Test;
+import ticTacToe.TicTacToe;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TicTacToeTest {
+
+    @Test
+    public void testThatUserCanMakeAMove(){
+        TicTacToe.play();
+        assertTrue();
+    }
+}
