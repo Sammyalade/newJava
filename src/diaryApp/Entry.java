ff5ea789b5358e576ca0c9c23c63ddc0d7ecedcc
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-07 00:53:42 +0100

    Tasks

--- src/diaryApp/Entry.java
+++ src/diaryApp/Entry.java
@@ -3,8 +3,10 @@ package diaryApp;
 public class Entry {
     private final int id;
     private String body;
+    private String title;
 
     public Entry(int id, String title, String body) {
+        this.title = title;
         this.id = id;
         this.body = body;
     }
@@ -15,5 +17,8 @@ public class Entry {
 
     public String getBody() {
         return body;
+    }
+    public String toString(){
+        return String.format("Title: %s%nBody: %s", this.title, this.body);
     }
 }
