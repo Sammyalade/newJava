93703f65ef0efba7118f5c14bfb6ab1580d9f008
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-10 13:50:41 +0100

    Tasks

--- src/eStore/User.java
+++ src/eStore/User.java
@@ -10,4 +10,45 @@ public class User {
     private Customer customer;
     private Seller seller;
     private Admin admin;
+
+    public User(int age, String emailAddress, String homeAddress, String name, String password, String phoneNumber, Customer customer, Seller seller, Admin admin) {
+        this.age = age;
+        this.emailAddress = emailAddress;
+        this.homeAddress = homeAddress;
+        this.name = name;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.customer = customer;
+        this.seller = seller;
+        this.admin = admin;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getCustomer() {
+        return customer.toString();
+    }
+
 }
