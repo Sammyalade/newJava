8067a93d7de7497a303f1b5ed12feaf7ffa0943b
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-10 11:12:19 +0100

    Tasks

--- src/eStore/CreditCardInformation.java
+++ src/eStore/CreditCardInformation.java
@@ -1,11 +1,45 @@
 package eStore;
 
+import java.time.LocalDate;
+
 public class CreditCardInformation {
 
-    private String cardCVV;
-    private String cardExpirationYear;
-    private String cardExpirationMonth;
-    private String cardNumber;
-    private String cardName;
-    private CardType cardType;
+    private final String cardCVV;
+    private final String cardExpirationYear;
+    private final String cardExpirationMonth;
+    private final String cardNumber;
+    private final String cardName;
+    private final CardType cardType;
+
+    public CreditCardInformation(String cardCVV, String cardExpirationYear, String cardExpirationMonth, String cardNumber, String cardName, CardType cardType) {
+        this.cardCVV = cardCVV;
+        this.cardExpirationYear = cardExpirationYear;
+        this.cardExpirationMonth = cardExpirationMonth;
+        this.cardNumber = cardNumber;
+        this.cardName = cardName;
+        this.cardType = cardType;
+    }
+
+    public String getCVV(){
+        return cardCVV;
+    }
+
+    public String getCardExpirationDate(){
+        LocalDate expirationDate = LocalDate.of(Integer.parseInt(cardExpirationYear), Integer.parseInt(cardExpirationMonth), 1);
+        return expirationDate.toString();
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+
 }
