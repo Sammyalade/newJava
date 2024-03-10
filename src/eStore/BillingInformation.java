b04b6a791b2c6be5c93857514f52b193a08ad313
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-10 11:12:53 +0100

    Tasks

--- src/eStore/BillingInformation.java
+++ src/eStore/BillingInformation.java
@@ -7,4 +7,27 @@ public class BillingInformation {
     private String receiverName;
     private  Address deliveryAddress;
     private CreditCardInformation cardInformation;
+
+    public BillingInformation(String receiverPhoneNumber, String receiverName, Address deliveryAddress, CreditCardInformation cardInformation) {
+        this.receiverPhoneNumber = receiverPhoneNumber;
+        this.receiverName = receiverName;
+        this.deliveryAddress = deliveryAddress;
+        this.cardInformation = cardInformation;
+    }
+
+    public String getReceiverPhoneNumber(){
+        return receiverPhoneNumber;
+    }
+
+    public String getDeliveryAddress(){
+        return deliveryAddress.toString();
+    }
+
+    public String getCardInformation(){
+        return cardInformation.toString();
+    }
+
+    public String getReceiverName(){
+        return receiverName;
+    }
 }
