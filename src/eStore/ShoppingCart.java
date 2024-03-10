9e39f4f96f41bc3fadc551286433d070dd105edf
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-10 13:50:53 +0100

    Tasks

--- src/eStore/ShoppingCart.java
+++ src/eStore/ShoppingCart.java
@@ -1,8 +1,20 @@
 package eStore;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-
     private List<Items> items;
+
+    public ShoppingCart(){
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Items newItem){
+        items.add(newItem);
+    }
+
+    public String getItems(){
+        return items.toString();
+    }
 }
