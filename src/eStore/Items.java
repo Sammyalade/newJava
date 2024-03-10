520d541a671a6a33c1d56b93db09f2146e637d73
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-10 11:12:42 +0100

    Tasks

--- src/eStore/Items.java
+++ src/eStore/Items.java
@@ -4,4 +4,17 @@ public class Items {
 
     private int quantityOfProductSelected;
     private Product product;
+
+    public Items(int quantityOfProductSelected, Product product){
+        this.quantityOfProductSelected = quantityOfProductSelected;
+        this.product = product;
+    }
+
+    public int returnQuantityOfProductSelected(){
+        return quantityOfProductSelected;
+    }
+
+    public Product returnProduct(){
+        return product;
+    }
 }
