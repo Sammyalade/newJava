379e36ed38c8cd11e250f92948e7adf36d9d2b39
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-10 14:42:37 +0100

    Tasks

--- src/eStore/CreditCardInformation.java
+++ src/eStore/CreditCardInformation.java
@@ -5,16 +5,14 @@ import java.time.LocalDate;
 public class CreditCardInformation {
 
     private final String cardCVV;
-    private final String cardExpirationYear;
-    private final String cardExpirationMonth;
+    private final LocalDate expirationDate;
     private final String cardNumber;
     private final String cardName;
     private final CardType cardType;
 
     public CreditCardInformation(String cardCVV, String cardExpirationYear, String cardExpirationMonth, String cardNumber, String cardName, CardType cardType) {
         this.cardCVV = cardCVV;
-        this.cardExpirationYear = cardExpirationYear;
-        this.cardExpirationMonth = cardExpirationMonth;
+        this.expirationDate = LocalDate.of(Integer.parseInt(cardExpirationYear), Integer.parseInt(cardExpirationMonth), 1);
         this.cardNumber = cardNumber;
         this.cardName = cardName;
         this.cardType = cardType;
@@ -25,7 +23,6 @@ public class CreditCardInformation {
     }
 
     public String getCardExpirationDate(){
-        LocalDate expirationDate = LocalDate.of(Integer.parseInt(cardExpirationYear), Integer.parseInt(cardExpirationMonth), 1);
         return expirationDate.toString();
     }
 
