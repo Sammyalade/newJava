7d7c8f7c60c0610b2c879749425bad38b3e92a2e
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-10 11:13:05 +0100

    Tasks

--- src/eStore/Product.java
+++ src/eStore/Product.java
@@ -7,4 +7,32 @@ public class Product {
     private int price;
     private String productDescription;
     private ProductCategory productCategory;
+
+    public Product(String productID, String productName, int price, String productDescription, ProductCategory productCategory) {
+        this.productID = productID;
+        this.productName = productName;
+        this.price = price;
+        this.productDescription = productDescription;
+        this.productCategory = productCategory;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
 }
