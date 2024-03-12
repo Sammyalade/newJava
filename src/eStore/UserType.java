8b1b26401106de6dcb43ee8157250f4dafcf743c
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-12 07:33:38 +0100

    Tasks

--- src/eStore/UserType.java
+++ src/eStore/UserType.java
@@ -1,4 +1,12 @@
 package eStore;
 
-public interface UserType {
+public class UserType {
+    public static User createUser(String userType, int age, String emailAddress, String homeAddress, String name, String password, String phoneNumber) {
+        return switch (userType.toLowerCase()) {
+            case "admin" -> new Admin(age, emailAddress, homeAddress, name, password, phoneNumber);
+            case "customer" -> new Customer(age, emailAddress, homeAddress, name, password, phoneNumber);
+            case "seller" -> new Seller(age, emailAddress, homeAddress, name, password, phoneNumber);
+            default -> throw new IllegalArgumentException("Invalid user type: " + userType);
+        };
+    }
 }
