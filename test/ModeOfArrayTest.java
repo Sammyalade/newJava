83277c8d1487d6a629b9920f595eda81ebd796a3
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-20 12:08:58 +0100

    Tasks

--- test/ModeOfArrayTest.java
+++ test/ModeOfArrayTest.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HighestOccuringTest {
+public class ModeOfArrayTest {
 
     @Test
     public void testThatReturnMostOccuring(){
         assertEquals(Arrays.toString(new int[]{3, 2}), ModeOfArray.checkForMode(1, 2, 2, 3, 4, 2));
-        assertEquals(Arrays.toString(new int[]{2, 3}), ModeOfArray.checkForMode(2, 3, 3, 5, 6, 7));
+        assertEquals(Arrays.toString(new int[]{3, 3}), ModeOfArray.checkForMode(3, 3, 3, 5, 6, 7));
     }
 }
