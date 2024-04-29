486eca91c1995af34cf0a787916e55e396389c8d
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-29 10:09:50 +0100

    Tasks

---
+++ src/NoAddition.java
@@ -0,0 +1,9 @@
+public class NoAddition {
+
+    public static int noPlusFunction(int number1, int number2) {
+        for(int i = number1; i <= number2; i++) {
+            number1++;
+        }
+        return number1;
+    }
+}
