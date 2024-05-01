d3fef7d87263c5b291e6b7d3ee96ef9086e001e0
Sammyalade <olufemialadeyelu@gmail.com>
2024-05-01 08:22:28 +0100

    Tasks

---
+++ src/json/serialization/Person.java
@@ -0,0 +1,41 @@
+package json.serialization;
+
+public class Person {
+
+    private String name;
+    private String dateOfBirth;
+    private String phoneNumber;
+    private Sex sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+}
