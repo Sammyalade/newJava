e7219e7d924e2061c5949c6c7d9c2369f08054c9
Sammyalade <olufemialadeyelu@gmail.com>
2024-05-01 08:22:00 +0100

    Tasks

---
+++ src/json/serialization/JsonSerializationTest.java
@@ -0,0 +1,22 @@
+package json.serialization;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JsonSerializationTest {
+
+    @Test
+    public void testJsonSerialization() {
+        Person person = new Person();
+        person.setName("Name");
+        person.setDateOfBirth("01/01/2001");
+        person.setSex(Sex.SHIM);
+        person.setPhoneNumber("09023456789");
+
+        String json =  JsonSerializer.serialize(person);
+        String expected = "{\"name\":\"Name\",\"dateOfBirth\":\"01/01/2001\",\"phoneNumber\":\"09023456789\",\"sex\":\"SHIM\"}";
+        assertEquals(expected, json);
+    }
+
+}
