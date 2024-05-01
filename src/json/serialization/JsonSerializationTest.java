4d9ba518aa50ea7789487048328242ab10b74589
Sammyalade <olufemialadeyelu@gmail.com>
2024-05-01 08:24:26 +0100

    Tasks

--- src/json/serialization/JsonSerializationTest.java
+++ src/json/serialization/JsonSerializationTest.java
@@ -2,7 +2,7 @@ package json.serialization;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonSerializationTest {
 
