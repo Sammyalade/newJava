3c68b519e50708e0f8f04c935bc9b9acab4f04bc
Sammyalade <olufemialadeyelu@gmail.com>
2024-05-09 09:59:21 +0100

    Tasks

--- src/json/serialization/JsonSerializationTest.java
+++ src/json/serialization/JsonSerializationTest.java
@@ -1,8 +1,14 @@
 package json.serialization;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static json.serialization.JsonSerializer.deserialize;
+import static json.serialization.JsonSerializer.serialize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class JsonSerializationTest {
 
@@ -10,13 +16,32 @@ public class JsonSerializationTest {
     public void testJsonSerialization() {
         Person person = new Person();
         person.setName("Name");
-        person.setDateOfBirth("01/01/2001");
+        person.setDateOfBirth(LocalDate.parse("2001-01-01"));
         person.setSex(Sex.SHIM);
         person.setPhoneNumber("09023456789");
 
-        String json =  JsonSerializer.serialize(person);
-        String expected = "{\"name\":\"Name\",\"dateOfBirth\":\"01/01/2001\",\"phoneNumber\":\"09023456789\",\"sex\":\"SHIM\"}";
+        String json =  serialize(person);
+        System.out.println(json);
+        String expected = "{\"name\":\"Name\",\"phoneNumber\":\"09023456789\",\"sex\":\"SHIM\",\"dob\":[2001,1,1]}";
         assertEquals(expected, json);
+    }
+
+
+    @Test
+    public void testDeserialization() throws JsonProcessingException {
+        Person person = new Person();
+        person.setName("Name");
+        person.setDateOfBirth(LocalDate.parse("2001-01-01"));
+        person.setSex(Sex.SHIM);
+        person.setPhoneNumber("09023456789");
+
+        String json =  serialize(person);
+        Person deserialize = deserialize(json);
+        assertNotNull(deserialize);
+        assertEquals("Name", deserialize.getName());
+        assertEquals(LocalDate.parse("2001-01-01"), deserialize.getDateOfBirth());
+        assertEquals("09023456789", deserialize.getPhoneNumber());
+        assertEquals(Sex.SHIM, deserialize.getSex());
     }
 
 }
